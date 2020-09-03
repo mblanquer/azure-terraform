8829396c25ffac49f1634d2da3095eeeb88674e2
mblanquer <matthias.blanquer@gmail.com>
2020-09-03 20:56:55 +0200

    Update provider.tf
    Clean code

--- Use_cases/PoC_Dynatrace/provider.tf
+++ Use_cases/PoC_Dynatrace/provider.tf
@@ -18,9 +18,9 @@ terraform {
     key                  = "terraform-network-__environment__.tfstate"
     access_key           = "__tf_storage_account_key__"*/
 
-  # storage_account_name = "pocdynatracesa01"
+  # storage_account_name = ""######"
   #   container_name       = "states"
   #   key                  = "terraform-dynatrace.tfstate"
-  #   access_key           = "157VAwZ6NK95UIpePTz5QPKVGC+xt4TvR45rA6rsKtpu8jD5Ffb1BqJa3J8K5HbHSvW2Q7aD5lZUgWnkgGrXLQ=="
+  #   access_key           = "######"
   # }
 }
